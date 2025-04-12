3c5ec1d22013156e00a9854ef95fe2317a717017
elik565 <eliseysavich@gmail.com>
2025-04-12 15:56:30 -0400

    Функция выполнения запроса

--- Server/database.hpp
+++ Server/database.hpp
@@ -3,11 +3,22 @@
 #include <libpq-fe.h>
 #include <string>
 
+// структура фильма
 struct Movie {
     int id;
     std::string title;
     int year;
 };
 
+// список запросов
+struct Queries {
+    std::string show_movie_list = "SELECT title, year FROM movies LIMIT 10";
+};
+
 // функция для подключения к базе данных
 PGconn* connect_to_db(const std::string& conn_info);
+
+// функция выполнения sql-запроса
+PGresult* execute_query(PGconn* conn, const std::string& query);
+
+
--- Server/server.cpp
+++ Server/server.cpp
@@ -11,4 +11,13 @@ int main(int argc, char* argv[]) {
     std::string conn_info = "dbname=EMovieDB host=localhost user=" + std::string(argv[1]) + " password=" + argv[2];
 
     PGconn* conn = connect_to_db(conn_info);  // подключаемся к бд
+    if (!conn) {
+        return 1;
+    }
+
+    Queries queries;  // список запросов
+
+    PGresult* res = execute_query(conn, queries.show_movie_list);
+
+    return 0;
 }
